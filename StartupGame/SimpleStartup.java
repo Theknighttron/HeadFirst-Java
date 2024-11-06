46e82be2885bb2e99a91e124a7312f867fd7732b
polyhistor2050 <chrisspaul2050@gmail.com>
2024-11-06 21:53:28 +0300

    Class setting hardcoded value for startup and checking user guess

---
+++ StartupGame/SimpleStartup.java
@@ -0,0 +1,30 @@
+class SimpleStartup {
+    private int[] locationCells;
+    private int numOfHits = 0;
+
+    public void setLocationCells(int[] locs) {
+        locationCells = locs;
+    }
+
+
+    public String checkYourself(int guess) {
+        String result = "miss";
+
+        // Check if the user guess if a hit
+        for (int cell: locationCells) {
+            if (guess == cell) {
+                result = "hit";
+                numOfHits++;
+                break;
+            }
+        }
+        // if hit = 3 it's a kill
+        if (numOfHits == locationCells.length) {
+            result = "kill";
+        }
+
+        // print the result
+        System.out.println(result);
+        return result;
+    }
+}
