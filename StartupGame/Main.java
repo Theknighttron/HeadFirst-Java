a07b937cd20394bbec1c2f315a66d817f4ea6c74
polyhistor2050 <chrisspaul2050@gmail.com>
2024-11-13 21:17:49 +0300

    Fix type errors from other classes used in the main class

--- StartupGame/Main.java
+++ StartupGame/Main.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 class Main {
     public static void main(String[] args) {
         int numOfGuesses = 0;
@@ -5,17 +7,21 @@ class Main {
         GameHelper helper = new GameHelper();
 
         // Set location and check if it's a hit or miss
-        SimpleStartup theStartup = new SimpleStartup();
+        Startup theStartup = new Startup();
 
         int randomNum = (int) (Math.random() * 5);
 
-        int[] locations = {randomNum, randomNum + 1, randomNum + 2};
+        ArrayList<String> locations = new ArrayList<>();
+        locations.add(String.valueOf(randomNum));
+        locations.add(String.valueOf(randomNum + 1));
+        locations.add(String.valueOf(randomNum + 2));
+
         theStartup.setLocationCells(locations);
 
         boolean isAlive = true;
 
         while(isAlive) {
-            int guess  = helper.getUserInput("Enter a number: ");
+            String guess = helper.getUserInput("Enter a number ");
             String result = theStartup.checkYourself(guess);
             numOfGuesses++;
 
@@ -24,6 +30,5 @@ class Main {
                 System.out.println("You took " + numOfGuesses + " guessess");
             }
         }
-
     }
 }
